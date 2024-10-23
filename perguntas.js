9f98b2c5d0ea508b92da18cfad105ff9f564bdbf
ricardobarros10 <ricardo.barros1036@gmail.com>
2024-10-23 17:24:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 124 dividido por 2?',
+    '62'
+)
+
+criaCartao(
+    'Biologia',
+    'Azeite, o que a vaca bebe?',
+    'Água:)'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'A Rússia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Matemática',
+    '12 x 4 = ?',
+    '48'
+)
+
+criaCartao(
+    'Artes',
+    'Quais as notas musicais?',
+    'Dó, ré, mi, fa, sol, lá, si'
+)
+
+criaCartao(
+    'Inglês',
+    'How are you?',
+    'I am doing good, thanks!'
+)
+
+criaCartao(
+    'Português',
+    'Onde está o verbo na frase: "ele caiu"?',
+    'Caiu'
+)
